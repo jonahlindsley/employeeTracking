71297e81bbde1f661835301b4a656f3e4b5a4458
jonah lindsley <jonahlindsley@yahoo.com>
2022-07-29 19:27:08 -0500

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name: VARCHAR(30) NOT NULL,
+    
+);
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title: VARCHAR(30) NOT NULL,
+    salary: DECIMAL,
+    department_id:INT
+    
+);
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id:INT,
+    manager_id:INT
+    );
